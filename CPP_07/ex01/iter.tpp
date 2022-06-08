830c68b1ea6a92b829639392776a53ff9bf7d684
Benoit Savinel <bsavinel@student.42.fr>
2022-06-08 15:45:39 +0200

    ajout CPP_07/ex01

--- CPP_07/ex01/iter.tpp
+++ CPP_07/ex01/iter.tpp
@@ -6,15 +6,15 @@
 /*   By: bsavinel <bsavinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 10:30:19 by bsavinel          #+#    #+#             */
-/*   Updated: 2022/06/07 18:45:15 by bsavinel         ###   ########.fr       */
+/*   Updated: 2022/06/08 14:48:56 by bsavinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_TPP
 # define ITER_TPP
 
-template <typename T>
-void iter(T *tab, int size, void (*f)(T))
+template <typename T, typename F, typename P>
+void iter(T *tab, int size, F (*f)(P))
 {
 	for (int i = 0; i < size; i++)
 	{
@@ -23,3 +23,9 @@ void iter(T *tab, int size, void (*f)(T))
 }
 
 #endif
+
+template <typename H>
+void ft_print(H &s)
+{
+    std::cout << s << std::endl;
+}
--- CPP_07/ex01/main.cpp
+++ CPP_07/ex01/main.cpp
@@ -6,13 +6,54 @@
 /*   By: bsavinel <bsavinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 10:28:44 by bsavinel          #+#    #+#             */
-/*   Updated: 2022/06/07 18:45:25 by bsavinel         ###   ########.fr       */
+/*   Updated: 2022/06/08 15:44:42 by bsavinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "iter.tpp"
 
+/*//! Main correction
+class Awesome
+{
+	public:
+		Awesome( void ) : _n( 42 )
+		{
+			return;
+		}
+		int get( void ) const
+		{
+			return this->_n;
+		}
+	private:
+		int _n;
+};
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+{
+	o << rhs.get();
+	return o;
+}
+
+template< typename T >
+void print(T const & x)
+{
+	std::cout << x << std::endl;
+	return;
+}
+
+int main() 
+{
+	int tab[] = { 0, 1, 2, 3, 4 };
+	Awesome tab2[5];
+
+	iter( tab, 5, print<int> );
+	iter( tab2, 5, print<Awesome> );
+
+	return 0;
+}
+*/
+
+//! Main perso
 
 void up_1(int &a)
 {
@@ -20,20 +61,59 @@ void up_1(int &a)
 	return ;
 }
 
+int up_2(const int &a)
+{
+	return a;
+}
+
 int main()
 {
-	int tab_int[8] = {0, 1, 2 ,3 ,4, 5, 6, 7};
-	for (int i = 0; i < 8; i++)
 	{
-		std::cout << tab_int[i] << " ";
-	}
-	std::cout << std::endl;
-	
-	iter(tab_int, 8, &up_1);
+		int tab_int[8] = {0, 1, 2 ,3 ,4, 5, 6, 7};
+		for (int i = 0; i < 8; i++)
+		{
+			std::cout << tab_int[i] << " ";
+		}
+		std::cout << std::endl;
 
-	for (int i = 0; i < 8; i++)
-	{
-		std::cout << tab_int[i] << " ";
+		iter(tab_int, 8, &up_1);
+
+		for (int i = 0; i < 8; i++)
+		{
+			std::cout << tab_int[i] << " ";
+		}
+		std::cout << std::endl;
 	}
-	std::cout << std::endl;
+	{
+		const int tab_int[8] = {0, 1, 2 ,3 ,4, 5, 6, 7};
+		for (int i = 0; i < 8; i++)
+		{
+			std::cout << tab_int[i] << " ";
+		}
+		std::cout << std::endl;
+
+		iter(tab_int, 8, &up_2);
+
+		for (int i = 0; i < 8; i++)
+		{
+			std::cout << tab_int[i] << " ";
+		}
+		std::cout << std::endl;
+	}
+	{
+		int tab_int[8] = {0, 1, 2 ,3 ,4, 5, 6, 7};
+		for (int i = 0; i < 8; i++)
+		{
+			std::cout << tab_int[i] << " ";
+		}
+		std::cout << std::endl;
+
+		iter(tab_int, 8, ft_print<int>);
+
+		for (int i = 0; i < 8; i++)
+		{
+			std::cout << tab_int[i] << " ";
+		}
+		std::cout << std::endl;
+	}
 }
