9516918a06fbe8296d4150eac9a66b54a7b3bf21
Benoit Savinel <bsavinel@student.42.fr>
2022-06-19 12:56:45 +0000

    main all test cpp02/ex02

--- CPP_02/ex02/main.cpp
+++ CPP_02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bsavinel <bsavinel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 17:09:51 by bsavinel          #+#    #+#             */
-/*   Updated: 2022/05/25 11:46:55 by bsavinel         ###   ########.fr       */
+/*   Updated: 2022/06/19 12:56:21 by bsavinel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,29 @@ int	main()
 {
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	Fixed c(2);
+	Fixed d(0.5f);
 	
+	std::cout << "increment opetarateur :" << std::endl;
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
 	std::cout << b << std::endl;
+	std::cout << "autre opetarateur :" << std::endl;
+	std::cout << c * d << std::endl;
+	std::cout << c / d << std::endl;
+	std::cout << c + d << std::endl;
+	std::cout << c - d << std::endl;
+	std::cout << (c == d) << std::endl;
+	std::cout << (c != d) << std::endl;
+	std::cout << (c >= d) << std::endl;
+	std::cout << (c <= d) << std::endl;
+	std::cout << (c > d)<< std::endl;
+	std::cout << (c < d) << std::endl;
 	
 	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << Fixed::min( a, b ) << std::endl;
 	return 0;
 }
